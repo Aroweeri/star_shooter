338ec3f248255b0d20a1f31d840f5fbb77709835
Dylan Sutton <dylanlsutton@gmail.com>
2019-10-22 22:01:43 -0400

    Use a global random number genenorator variable.
    We do this so that we aren't making a new variable for each star.
    Also use the same global rng in the main world script.

--- star_shooter/Light2D.gd
+++ star_shooter/Light2D.gd
@@ -4,9 +4,7 @@ var starRotation
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var rng = RandomNumberGenerator.new()
-	rng.randomize()
-	starRotation = rng.randf_range(-0.10, 0.10)
+	starRotation = global.rng.randf_range(-0.10, 0.10)
 
 func _process(delta):
 	rotate(starRotation)
